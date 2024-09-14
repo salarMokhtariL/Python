c947d9d56bba570aa47ad3255da1d1f596424730
Salar Mokhtari Laleh <75142232+salarMokhtariL@users.noreply.github.com>
2024-09-14 15:38:28 +0800

    Chapter 2

---
+++ CH2_Multipul Assignment.py
@@ -0,0 +1,37 @@
+'''
+Chapter 2 : Multiple Assignment 
+    Multiple assignment in Python allows you to assign values to multiple variables simultaneously in a single line of code. This can make code more concise and readable.
+'''
+
+name = 'XXX'
+age = 25
+attractive = True
+
+print(name)
+print(age)
+print(attractive)
+
+
+Mname, Mage, Mattractive = 'YYY', 24, False
+
+print(Mname)
+print(Mage)
+print(Mattractive)
+
+
+A = 1
+B = 1
+C = 1
+D = 1
+
+print(A)
+print(B)
+print(C)
+print(D)
+
+A_1 = B_1 = C_1 = D_1 = 1
+
+print(A_1)
+print(B_1)
+print(C_1)
+print(D_1)
